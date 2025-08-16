c9a3bdabc4014fbb9d50d0031977261ff230364d
Fercho Quintero <faq.sau@gmail.com>
2025-08-16 11:05:34 -0500

    Se quita código que no se usa

--- src/main/java/co/com/screenplay/project/utils/ApiConstants.java
+++ src/main/java/co/com/screenplay/project/utils/ApiConstants.java
@@ -3,8 +3,5 @@ package co.com.screenplay.project.utils;
 public class ApiConstants {
 
     public static final String BASE_URL = "https://restful-booker.herokuapp.com";
-    public static final String AUTH_ENDPOINT = "/auth/login";
-    public static final String USERS_ENDPOINT = "/users";
-    public static final String PRODUCTS_ENDPOINT = "/products";
 
 }
